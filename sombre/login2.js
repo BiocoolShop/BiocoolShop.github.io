d87104ac5b560baa4e4fdce8e004fb9c67ea4413
BiocoolShop <84512199+BiocoolShop@users.noreply.github.com>
2021-05-20 18:17:07 +0200

    Update login2.js

--- sombre/login2.js
+++ sombre/login2.js
@@ -3,12 +3,10 @@ function validate()
 var password=document.getElementById("password").value;
 if(password=="user")
 {
-    alert("login succesfully");
-    return false;
-}
-else
-{
-    alert("login failed");
+    alert("Mot De passe correct ! ");
+    return true;
+}else{
+    do {    alert("Mauvais mot de passe refresh la page !"); } while (true);
 }
 
 
